11dcb5262c9eee0b3e70c50e5a9dbd62e1dd5e8d
Aung Ye Zaw <aungyezaw16316@gmail.com>
2020-09-07 16:14:42 +0630

    clones

---
+++ News-App/app/src/main/java/com/azp/newsapp/extensions/Extensions.kt
@@ -0,0 +1,18 @@
+package com.azp.newsapp.extensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+fun toSimpleString(publishedAt: String): String {
+    val inputFormat = SimpleDateFormat(
+        "yyyy-MM-dd'T'HH:mm:ss'Z'",
+        Locale.ENGLISH
+    )
+    val outputFormat = SimpleDateFormat(
+        "EEE, dd MMM yyyy",
+        Locale.getDefault()
+    )
+    val date = inputFormat.parse(publishedAt)
+    return outputFormat.format(date)
+}
